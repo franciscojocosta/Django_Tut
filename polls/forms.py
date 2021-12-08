72caa25233f012f8368cbe8d724876172f2f427f
Francisco Costa <franciscojocosta@gmail.com>
2021-12-08 20:32:47 +0000

    Simplified Project - New Path for files

--- polls/forms.py
+++ polls/forms.py
@@ -1,10 +1,2 @@
-
 from django import forms
 
-from .models import Book
-
-
-class BookForm(forms.ModelForm):
-    class Meta:
-        model = Book
-        fields = ('title', 'author', 'pdf', 'cover')
